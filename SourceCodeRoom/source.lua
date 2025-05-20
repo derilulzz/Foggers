51b707d2746cadae1ee74c755c51cabbacfdecc7
Deri LULZZ <deniapk0078@gmail.com>
2025-05-20 00:06:13 -0300

    TRANSLATE

--- SourceCodeRoom/source.lua
+++ SourceCodeRoom/source.lua
@@ -2,12 +2,21 @@ function createSourceInfo()
     local sInf = {
         licence = "MIT-licence",
         gitUrl = "https://github.com/derilulzz/Foggers",
-        openGitButton = createButton(800 / 2, 600 / 2, 128, 64, "Goto Source Code", "Open the git web page of the project", true),
+        openGitButton = createButton(800 / 2, 600 / 2, 128, 64, "Go to Source Code", "Open the git web page of the project", true),
         returnButton = createButton(800 / 2, (600 / 2) + 64 + 8, 128, 64, "Return", "", true),
     }
 
 
     function sInf:update()
+        if gameStuff.lang == "pt-br" then
+            self.openGitButton.text = "Ir para o source code"
+            self.openGitButton.addText = "Abrir a pagina da web do git aonde o codigo do jogo está"
+        else
+            self.openGitButton.text = "Go to Source Code"
+            self.openGitButton.addText = "Open the git web page of the project"
+        end
+
+
         if self.openGitButton.pressed then
             love.system.openURL(self.gitUrl)
             self.openGitButton.pressed = false
@@ -24,7 +33,9 @@ function createSourceInfo()
 
 
         drawOutlinedText("Source Code", 800 / 2, 16 + 8, 0.1 * math.sin(GlobalSinAngle), 4, 4, nil, nil, 2, {0, 0, 0})
-        drawOutlinedTextF("All the game's code is under the MIT licence, that means you can do whatever you want with the game, the only thing you CANT do is say that you created the original game", 800 / 2, 32 + 64, 800 / 2, "center", 0.01 * math.sin(GlobalSinAngle), 2, 2, nil, nil, 2, {0, 0, 0})
+        local text = "All the game's code and everything else is under the MIT licence, that means you can do almost whatever you want with the game, the only thing you CANT do is say that you created the original game"
+        if gameStuff.lang == "pt-br" then text = "Todo o jogo está sobre a MIT licence, isso significa que você pode fazer quase tudo oque você quiser com o jogo, a unica coisa que você não pode fazer é falar que criou o jogo original" end
+        drawOutlinedTextF(text, 800 / 2, 32 + 64, 800 / 2, "center", 0.01 * math.sin(GlobalSinAngle), 2, 2, nil, nil, 2, {0, 0, 0})
     end
 
 
